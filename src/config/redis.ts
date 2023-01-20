92d63a430861f0d37d81b2832905991da9a9ed91
Master_Shifu <sheriff0613@gmail.com>
2023-01-21 00:20:01 +0800

    modified redis handler

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -3,9 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const redisEndpoint = process.env.REDIS_ENDPOINT;
-const redisUserName = process.env.REDIS_USERNAME;
-const redisPassword = process.env.REDIS_PASSWORD;
+// const redisEndpoint = process.env.REDIS_ENDPOINT;
+// const redisUserName = process.env.REDIS_USERNAME;
+// const redisPassword = process.env.REDIS_PASSWORD;
+
+const redisEndpoint =
+    'redis-17092.c10.us-east-1-4.ec2.cloud.redislabs.com:17092';
+const redisUserName = 'default';
+const redisPassword = 'uLeu2IEukbDx9X4Sxv0ISupSNM3sZdTq';
 
 console.log('here', { redisEndpoint, redisUserName, redisPassword });
 
