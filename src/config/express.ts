b5afeb941d138325ae05bfa72d423828ef0969fe
Master_Shifu <sheriff0613@gmail.com>
2023-01-20 20:36:17 +0800

    modified for deploying to vercel

--- src/config/express.ts
+++ src/config/express.ts
@@ -5,6 +5,7 @@ import { routes } from '../routes';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
