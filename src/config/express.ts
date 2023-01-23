3c81d87827824a5a908f30d33f389583b4858f2e
Master_Shifu <sheriff0613@gmail.com>
2023-01-23 17:10:46 +0800

    modified package.json and app object

--- src/config/express.ts
+++ src/config/express.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import { routes } from '../routes';
+import bodyParser from 'body-parser';
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
