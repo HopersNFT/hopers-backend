4f474d359f237e3a7fb2231efac7ff16bdbb4f1c
Marius Modlich <marius@modlich.de>
2023-05-12 01:00:34 +0200

    removed rp

--- src/config/express.ts
+++ src/config/express.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import morgan from 'morgan';
 import { routes } from '../routes';
 import bodyParser from 'body-parser';
-import rp from 'request-promise'
 
 const app = express();
 app.use(express.static('public'));
@@ -12,10 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
-app.use((_req, _res, next) => {
-    rp('https://hopers-backend-v2-psi.vercel.app/')
-    next();
-})
 app.use('/', routes);
 app.disable('x-powered-by');
 
